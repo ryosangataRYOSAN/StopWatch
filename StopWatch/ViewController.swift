64c1189d0414d6b90258cf38dfd2294d9799407a
ryohei suzuki <ryousanngataryousann@gmail.com>
2018-09-17 09:15:32 +0900

    finish

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label : UILabel!
+    @IBOutlet var hanteiLabel :UILabel!
+    
+    var count : Float = 0.0
+    
+    var timer : Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +28,57 @@ class ViewController: UIViewController {
     }
 
 
+    
+    @objc func up(){
+        print("呼び出し")
+        count = count + 0.01
+        label.text = String(format : "%.2f",count)
+    }
+    
+    
+    @IBAction func start(){
+        if !timer.isValid{
+            timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                         target : self,
+                                         selector : #selector(self.up),
+                                         userInfo : nil,
+                                         repeats : true
+            )
+        }
+    }
+    
+    @IBAction func stop(){
+        if timer.isValid{
+            timer.invalidate()
+            hantei()
+        }
+    }
+    
+    @IBAction func reset(){
+        if timer.isValid{
+            stop()
+        }
+        count = 0
+        label.text = "0"
+        hanteiLabel.text = ""
+    }
+    
+    func hantei(){
+        if fabs(count-10.0) < 0.2{
+            hanteiLabel.text = "PERFECT!"
+            hanteiLabel.textColor = UIColor.red
+        }else if fabs(count-10.0) < 0.3{
+            hanteiLabel.text = "GREAT!"
+            hanteiLabel.textColor = UIColor.orange
+        }else if fabs(count-10.0) < 0.5{
+            hanteiLabel.text = "GOOD!"
+            hanteiLabel.textColor = UIColor.blue
+        }else{
+            hanteiLabel.text = "BAD"
+            hanteiLabel.textColor = UIColor.black
+        }
+    }
+    
+    
 }
 
